cbda20c022b0aec2451c724dbe76cc2a39782d7e
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-11-04 11:15:25 -0300

    get all matches in the year on 1st and 2nd divisions

--- matches_crawler.py
+++ matches_crawler.py
@@ -2,25 +2,42 @@ from requests import get
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 
-url = "https://globoesporte.globo.com/futebol/brasileirao-serie-a/"
+url_serie_a = "https://globoesporte.globo.com/futebol/brasileirao-serie-a/"
+url_serie_b = "https://globoesporte.globo.com/futebol/brasileirao-serie-b/"
 
 ff = webdriver.Firefox()
-ff.get(url)
+ff.get(url_serie_a)
 bs_obj = bs(ff.page_source, 'html.parser')
 
-boxes = bs_obj.find_all('div', {'class' :'jogo theme'})
+setas = ff.find_elements_by_class_name('lista-jogos__navegacao--setas')
+seta_esq = setas[0]
+seta_dir = setas[1]
 
-for box in boxes:
-    mandante = box.find('div', {'class':'placar__equipes placar__equipes--mandante'})
-    mandante = mandante.find('span', {'class':'equipes__nome'}).text
-    
-    visitante = box.find('div', {'class':'placar__equipes placar__equipes--visitante'})
-    visitante = visitante.find('span', {'class':'equipes__nome'}).text
-    
-    info = box.find('div', {'class':'jogo__informacoes'})
-    data = info.find('span', {'class':'jogo__informacoes--data'}).text.strip()
-    local = info.find('span', {'class':'jogo__informacoes--local'}).text.strip()
-    hora = info.find('span', {'class':'jogo__informacoes--hora'}).text.strip()
+for i in range(38):
+    seta_esq.click()
 
-    print('\n{} x {}\n{} {} {}\n'.format(mandante, visitante, data, hora, local))
+for i in range(38):
+    
+    bs_obj = bs(ff.page_source, 'html.parser')
+
+    rodada = bs_obj.find('span', {'class':'lista-jogos__navegacao--rodada'})
+    print(rodada.text)
+    
+    boxes = bs_obj.find_all('div', {'class' :'jogo theme'})
+    for box in boxes:
+        mandante = box.find('div', {'class':'placar__equipes placar__equipes--mandante'})
+        mandante = mandante.find('span', {'class':'equipes__nome'}).text
+        
+        visitante = box.find('div', {'class':'placar__equipes placar__equipes--visitante'})
+        visitante = visitante.find('span', {'class':'equipes__nome'}).text
+        
+        info = box.find('div', {'class':'jogo__informacoes'})
+        data = info.find('span', {'class':'jogo__informacoes--data'}).text.strip()
+        local = info.find('span', {'class':'jogo__informacoes--local'}).text.strip()
+        hora = info.find('span', {'class':'jogo__informacoes--hora'}).text.strip()
+
+        print('\n{} x {}\n{} {} {}\n'.format(mandante, visitante, data, hora, local))
+
+    seta_dir.click()
+
 
