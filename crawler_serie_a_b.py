af05915fe54ffc2d84dfda10c52394e5edf91303
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-11-12 16:44:23 -0300

    Translate 'crawler series a e b'  variables name to english

--- crawler_serie_a_b.py
+++ crawler_serie_a_b.py
@@ -10,35 +10,35 @@ ff = webdriver.Firefox()
 ff.get(url_serie_a)
 bs_obj = bs(ff.page_source, 'html.parser')
 
-setas = ff.find_elements_by_class_name('lista-jogos__navegacao--setas')
-seta_esq = setas[0]
-seta_dir = setas[1]
+arrows = ff.find_elements_by_class_name('lista-jogos__navegacao--setas')
+left_arrow = arrows[0]
+right_arrow = arrows[1]
 
 for i in range(38):
-    seta_esq.click()
+    left_arrow.click()
 
 for i in range(38):
 
     bs_obj = bs(ff.page_source, 'html.parser')
 
-    rodada = bs_obj.find('span', {'class':'lista-jogos__navegacao--rodada'})
-    print(rodada.text)
+    round = bs_obj.find('span', {'class':'lista-jogos__navegacao--rodada'})
+    print(round.text)
     
     boxes = bs_obj.find_all('div', {'class' :'jogo theme'})
     for box in boxes:
-        mandante = box.find('div', {'class':'placar__equipes placar__equipes--mandante'})
-        mandante_sigla = mandante.find('span', {'class', 'equipes__sigla'}).text
-        mandante = mandante.find('span', {'class':'equipes__nome'}).text
+        home_team = box.find('div', {'class':'placar__equipes placar__equipes--mandante'})
+        home_team_sigla = home_team.find('span', {'class', 'equipes__sigla'}).text
+        home_team = home_team.find('span', {'class':'equipes__nome'}).text
 
-        visitante = box.find('div', {'class':'placar__equipes placar__equipes--visitante'})
-        visitante_sigla = visitante.find('span', {'class', 'equipes__sigla'}).text
-        visitante = visitante.find('span', {'class':'equipes__nome'}).text
+        guest_team = box.find('div', {'class':'placar__equipes placar__equipes--visitante'})
+        guest_team_sigla = guest_team.find('span', {'class', 'equipes__sigla'}).text
+        guest_team = guest_team.find('span', {'class':'equipes__nome'}).text
         
         info = box.find('div', {'class':'jogo__informacoes'})
-        data = info.find('span', {'class':'jogo__informacoes--data'}).text.strip()
+        date = info.find('span', {'class':'jogo__informacoes--data'}).text.strip()
         local = info.find('span', {'class':'jogo__informacoes--local'}).text.strip()
-        hora = info.find('span', {'class':'jogo__informacoes--hora'}).text.strip()
+        time = info.find('span', {'class':'jogo__informacoes--hora'}).text.strip()
 
-        print('\n{} x {}\n{} {} {}\n'.format(mandante, visitante, data, hora, local))
+        print('\n{} x {}\n{} {} {}\n'.format(home_team, guest_team, date, time, local))
 
-    seta_dir.click()
+    right_arrow.click()
