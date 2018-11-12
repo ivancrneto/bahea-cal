9b591ca60dc577e30c16114c12e60bfd915702f1
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-11-12 16:40:50 -0300

    Translate 'crawler copa do brasil'  variables name to english

--- crawler_copa_brasil.py
+++ crawler_copa_brasil.py
@@ -7,15 +7,15 @@ def print_matches(boxes):
     for box in boxes:
         info = box.find('div', {'class':'placar-jogo-informacoes'}).text
 
-        mandante = box.find('span', {'class':'placar-jogo-equipes-item placar-jogo-equipes-mandante'})
-        mandante_sigla = mandante.find('span', {'class':'placar-jogo-equipes-sigla'}).text
-        mandante = mandante.find('span', {'class':'placar-jogo-equipes-nome'}).text
+        home_team = box.find('span', {'class':'placar-jogo-equipes-item placar-jogo-equipes-mandante'})
+        home_team_sigla = home_team.find('span', {'class':'placar-jogo-equipes-sigla'}).text
+        home_team = home_team.find('span', {'class':'placar-jogo-equipes-nome'}).text
 
-        visitante = box.find('span', {'class':'placar-jogo-equipes-item placar-jogo-equipes-visitante'})
-        visitante_sigla = visitante.find('span', {'class':'placar-jogo-equipes-sigla'}).text
-        visitante = visitante.find('span', {'class':'placar-jogo-equipes-nome'}).text
+        guest_team = box.find('span', {'class':'placar-jogo-equipes-item placar-jogo-equipes-visitante'})
+        guest_team_sigla = guest_team.find('span', {'class':'placar-jogo-equipes-sigla'}).text
+        guest_team = guest_team.find('span', {'class':'placar-jogo-equipes-nome'}).text
         
-        output = '{} x {}\n{}\n'.format(mandante, visitante, info)
+        output = '{} x {}\n{}\n'.format(home_team, guest_team, info)
         print(output)
 
 base_url = "https://globoesporte.globo.com/futebol"
@@ -25,22 +25,22 @@ ff = webdriver.Firefox()
 ff.get(url_copa_brasil)
 bs_obj = bs(ff.page_source, 'html.parser')
 
-seta_fase_esq = ff.find_element_by_class_name('gui-icon-arrow-left-highlight')
-seta_fase_dir = ff.find_element_by_class_name('gui-icon-arrow-right-highlight')
+left_arrow = ff.find_element_by_class_name('gui-icon-arrow-left-highlight')
+right_arrow = ff.find_element_by_class_name('gui-icon-arrow-right-highlight')
 
 for i in range(8):
-    seta_fase_esq.click()
+    left_arrow.click()
     sleep(1)
 
 for i in range(8):
     sleep(1)
     bs_obj = bs(ff.page_source, 'html.parser')
-    fase = bs_obj.find('span', {'class':'tabela-navegacao-seletor-ativo'}).text
-    print(fase, "\n")
+    phase = bs_obj.find('span', {'class':'tabela-navegacao-seletor-ativo'}).text
+    print(phase, "\n")
 
     boxes = bs_obj.find_all('div', {'class':'placar-jogo'})
 
     print_matches(boxes)
 
-    seta_fase_dir.click()
+    right_arrow.click()
     
