0a54fc48d718bc8a225fb58021fb27744eda90b6
kauan nery <113049473+Kauannery@users.noreply.github.com>
2024-05-10 15:40:50 -0300

    feat: Envia access token pro backend

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -20,25 +20,41 @@ export default class Login extends React.Component {
     this.handleItemClick = this.handleItemClick.bind(this);
   }
 
- handleItemClick = async (event, name) => {
+ 
+  handleItemClick = async (event, name) => {
     
 
-    if (name === 'sign-in') {
-        try {
-            await apiCalendar.handleAuthClick();
-           
-            // Ou qualquer outra ação após o login bem-sucedido
-        } catch(error){
-            //console.error('erro ocorrido' , error);
+   if (name === 'sign-in') {
+         {
+            const  myResponse = await apiCalendar.handleAuthClick()
+            console.log(myResponse.access_token)
+
+           /* var meuDicionario = {}
+            for (var key in rs1) {
+              meuDicionario[key] = rs1[key];
+            }
+            console.log(meuDicionario)
+            console.log(meuDicionario.access_token)
+*/
+            const response =  fetch('/v1/calendar/flow/', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                // Supondo que você precise passar o access token no corpo da requisição
+                // Modifique isso de acordo com os requisitos da sua API
+              },
+             body: JSON.stringify({ accessToken: myResponse.access_token }),                
+            });
+            console.log(response.body)
+         }
             
-        }
+         }
+         else if (name === 'sign-out') {
+          apiCalendar.handleSignoutClick();
+         }
+    }
+      
 
-       } else if (name === 'sign-out') {
-        apiCalendar.handleSignoutClick();
-    } 
-};
-
-  
   render() {
     return (
       <div>
@@ -58,3 +74,21 @@ export default class Login extends React.Component {
   }
 }
   
+
+
+ /* const response = await fetch('/v1/calendar/flow/', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                // Supondo que você precise passar o access token no corpo da requisição
+                // Modifique isso de acordo com os requisitos da sua API
+              },
+             // body: JSON.stringify({ accessToken: access_token }),
+            });
+            
+            // Verificar se a chamada à API foi bem-sucedida
+            if (!response.ok) {
+              throw new Error('Erro ao chamar a API do backend');
+            }
+           */
+            // Ou qualquer outra ação após o login bem-sucedido
