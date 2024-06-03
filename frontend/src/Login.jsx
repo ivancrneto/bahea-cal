9ddbccd674fb643a6ded8ac7f7652ff7bf52eca1
kauan nery <113049473+Kauannery@users.noreply.github.com>
2024-06-03 15:51:35 -0300

    Revert "Revert "alterações 29/05""
    This reverts commit 06acda7a9d2d7e61edef6b1082bcdfdb3c69056c.

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,7 +1,6 @@
 
 import axios from 'axios';
 
-// const apiCalendar = new ApiCalendar(config);
 export default function Login() {
   
   <GoogleOAuthProvider clientId="470653035644-rkr19rof1eclp7f7gmd4044jt110hf9g.apps.googleusercontent.com"></GoogleOAuthProvider>;
@@ -49,77 +48,6 @@ export default function Login() {
             onSuccess={handleLogin}
         />
     </GoogleOAuthProvider>)
-=======
-  // // await axios(config)
-  //   const response = fetch('http://localhost:8000/api/v1/calendar/token/', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Allow-Control-Allow-Origin': '*'
-  //     },
-  //     body: data,                
-  //   });
-  //   console.log('response body =', response.body)
-
-  //   const hasAccess = hasGrantedAllScopesGoogle(
-  //     response.body,
-  //     "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.app.created",
-  //     "https://www.googleapis.com/auth/userinfo.email",
-  //     "https://www.googleapis.com/auth/userinfo.profile",
-  //     "openid"
-  //   );  
-  //   console.log('hasAccess = ', hasAccess);
-  // }
-
-  const handleLogin = async(credentialResponse) => {
-    // console.log('credential Response', credentialResponse)
-    const response = fetch('http://localhost:8000/api/v1/calendar/token/', {
-      method: 'POST',
-      // mode: 'no-cors',
-      headers: {
-        // 'X-BLA' : 'bla!',
-        'Access-Control-Allow-Headers' : '*',
-        'Content-Type': 'application/json',
-        'Allow-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': '*'
-      },
-      body: credentialResponse,                
-    });
-    
-    // console.log('response body =', response.body)
-    console.log(credentialResponse)
-  }
-  // const teste = async() => {
-  //   const resposta = fetch('http://localhost:8000/api/v1/calendar/token/', {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Access-Control-Allow-Headers': 'X-Requested-With',
-  //       'Allow-Control-Allow-Origin': '*' 
-  //     },
-  //   });
-  //   console.log('resposta = ',resposta);
-  // }
-
-  const login = useGoogleLogin({ 
-    scope: "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.app.created https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid", 
-    flow: 'auth-code',
-    access_type: 'offline',
-    prompt: 'consent',
-    onSuccess: handleLogin,
-  });
-
-  // return (<button onClick={() => teste()}> oiasodiaosio</button>)
-  return (<button onClick={() => login()}> Fazer login</button>)
-
-  // return(<GoogleLogin onSuccess={credentialResponse => {
-  //     console.log(credentialResponse);
-  //   }}
-  //   onError={() => {
-  //     console.log('Login Failed');
-  //   }}
-  // />) 
->>>>>>> da576de (tests code)
 }
 
 
